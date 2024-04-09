ea565cb944f8f1a0e7e79b9ba818754cf5a48ca2
tseijp <_@tsei.jp>
2024-04-09 15:35:34 +0900

    Initial commit

---
+++ test/cdk-lambda-jwt.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaJwt from '../lib/cdk-lambda-jwt-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-jwt-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaJwt.CdkLambdaJwtStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
